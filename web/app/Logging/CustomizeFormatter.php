82cf784b12dbd7823205ec9c2959e11cdd121675
yoneshu-pro <r.ta0.p2008@i.softbank.jp>
2018-12-03 01:01:46 +0900

    phpDocの追加

--- web/app/Logging/CustomizeFormatter.php
+++ web/app/Logging/CustomizeFormatter.php
@@ -10,32 +10,40 @@ use Monolog\Processor\ProcessIdProcessor;
 
 class CustomizeFormatter
 {
-   private $logFormat = '[%datetime% %channel%.%level_name% ip:%extra.ip% pid:%extra.process_id% %extra.class%::%extra.function%(%extra.line%)] %message% %context%' . PHP_EOL;
 
-   private $dateFormat = 'Y/m/d H:i:s.v';
+    /**
+     * ログのフォーマット
+     * @var string
+     */
+    private $logFormat = '[%datetime% %channel%.%level_name% ip:%extra.ip% pid:%extra.process_id% %extra.class%::%extra.function%(%extra.line%)] %message% %context%' . PHP_EOL;
 
-   /**
-    * Customize the given Monolog instance.
-    *
-    * @param  \Monolog\Logger  $monolog
-    *
-    * @return void
-    */
-   public function __invoke($monolog)
-   {
-       $formatter = new LineFormatter($this->logFormat, $this->dateFormat, true, true);
+    /**
+     * 日付のフォーマット
+     * @var string
+     */
+    private $dateFormat = 'Y/m/d H:i:s.v';
 
-       $ip = new IntrospectionProcessor(Logger::DEBUG, ['Illuminate\\']);
+    /**
+     * 渡されたロガーインスタンスのカスタマイズ
+     * 
+     * @param \Illuminate\Log\Logger  $logger
+     * @return void
+     */
+    public function __invoke($monolog)
+    {
+        $formatter = new LineFormatter($this->logFormat, $this->dateFormat, true, true);
 
-       $wp = new WebProcessor();
+        $ip = new IntrospectionProcessor(Logger::DEBUG, ['Illuminate\\']);
 
-       $pid = new ProcessIdProcessor();
+        $wp = new WebProcessor();
 
-       foreach ($monolog->getHandlers() as $handler) {
-           $handler->setFormatter($formatter);
-           $handler->pushProcessor($ip);
-           $handler->pushProcessor($wp);
-           $handler->pushProcessor($pid);
-       }
-   }
+        $pid = new ProcessIdProcessor();
+
+        foreach ($monolog->getHandlers() as $handler) {
+            $handler->setFormatter($formatter);
+            $handler->pushProcessor($ip);
+            $handler->pushProcessor($wp);
+            $handler->pushProcessor($pid);
+        }
+    }
 }
